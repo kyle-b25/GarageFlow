c3f00f1ececa78e13b0a0a81239dc9099d4c1068
Sam Gibney <samuel.gibney@gmail.com>
2026-03-16 23:19:34 -0400

    Added new folders and created /operator-front extension

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
@@ -10,6 +10,10 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
 db = SQLAlchemy(app)
+
+@app.route('/operator-front')
+def index():
+    return render_template('index.html')
 
 @app.route('/health')
 def health():
